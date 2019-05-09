b3f8191deb7b21816f72e954f8006cb32f69c093
reddingk <kris.redding3@gmail.com>
2019-05-08 21:10:06 -0400

    init branch

---
+++ server/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    remoteUrl : 'mongodb+srv://GAli_Admin:GAli_Base@cluster0-lc06u.mongodb.net/gali-site?retryWrites=true',
+    dbName: 'gali-site',
+    mongoOptions: { connectTimeoutMS: 2000, socketTimeoutMS: 2000}
+};

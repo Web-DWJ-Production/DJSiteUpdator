fd122d65398caabeb02f4dc3c224362cc4622a40
reddingk <kris.redding3@gmail.com>
2019-07-19 07:55:56 -0400

    update ministry

--- src/js/ministries.js
+++ src/js/ministries.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import defaultImg from "../assets/imgs/amez_logo.png";
+
 class Ministries extends Component{
     constructor(props) {
         super(props);
@@ -19,8 +21,21 @@ class Ministries extends Component{
                 <div className="split-editor">
                     <div className="song-selector split">
                         <div className="music-list">
-                        {this.state.ministryList.map((song,i) => (
-                            <div className="ministry-section"></div>
+                        {this.state.ministryList.map((section,i) => (
+                            <div className="ministry-section" key={i}>
+                                <div className="sectionTitle">{section.sectionTitle}</div>
+
+                                <div className="ministry-item-list">
+                                    {section.list.map((item,k) => (
+                                        <div key={i} className="music-item">
+                                            <div className="music-icon">                                                    
+                                                <img src={(item.logo && item.logo !== "" ? item.logo : defaultImg)}/>
+                                            </div>
+                                            <div className="music-title">{item.title}</div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
                         ))}
                         </div>
                     </div>
@@ -38,7 +53,7 @@ class Ministries extends Component{
     loadMinistries(){
         var self = this;
         try {
-            fetch(self.props.baseUrl + "/api/getAllMinistries")
+            fetch(self.props.baseUrl + "/api/getMinistries")
             .then(function(response) {
                 if (response.status >= 400) {
                   throw new Error("Bad response from server");
